f97b59e3634619d39aaf96c834494994892efd42
behemoth-truongnd <truongnd@behemoth.vn>
2022-09-28 14:37:07 +0700

    add seed create user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def initialize_master_table(*models)
+  models.each do |model|
+    model.delete_all
+    model.connection.execute("alter table #{model.table_name} AUTO_INCREMENT = 1;")
+  end
+end
+
+initialize_master_table(User)
+User.create!(email: "test@example.com", password: "aA123456@", password_confirmation: "aA123456@")
